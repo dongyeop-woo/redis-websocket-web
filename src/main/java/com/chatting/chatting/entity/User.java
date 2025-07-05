a6106ea72f96f104599d73c8d486586fb7e7eb34
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-05 23:47:09 +0900

    feat: 유저 기본 세팅

---
+++ src/main/java/com/chatting/chatting/entity/User.java
@@ -0,0 +1,22 @@
+package com.chatting.chatting.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String nickname;
+    private String password;
+}
