5107645c4cae0c71c53ed91063a64a6efaeab016
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-06 19:34:41 +0900

    feat: 닉네임 조회 필드 추가

--- src/main/java/com/chatting/chatting/repository/UserRepository.java
+++ src/main/java/com/chatting/chatting/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.chatting.chatting.repository;
 
-import org.apache.catalina.User;
+import com.chatting.chatting.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByNickname(String nickname);
 }
