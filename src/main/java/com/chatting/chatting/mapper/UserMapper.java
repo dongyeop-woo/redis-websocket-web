ad3cb94077606fc9499c037cdc9b44977c52f5f0
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-06 19:34:28 +0900

    feat: 인터페이스로 변경 -> Mapstruct 사용 후 필드 추가

--- src/main/java/com/chatting/chatting/mapper/UserMapper.java
+++ src/main/java/com/chatting/chatting/mapper/UserMapper.java
@@ -1,4 +1,11 @@
 package com.chatting.chatting.mapper;
 
-public class UserMapper {
+import com.chatting.chatting.dto.UserDTO;
+import com.chatting.chatting.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDTO toUserDTO(User user);
+    User toUser(UserDTO userDTO);
 }
