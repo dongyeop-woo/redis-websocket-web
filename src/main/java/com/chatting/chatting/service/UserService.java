3df155580524520105b4494ec82d6ecc62c8e1f2
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-06 19:35:11 +0900

    feat: 유저 생성, 전체 조회, 닉네임으로 조회 서비스 생성

--- src/main/java/com/chatting/chatting/service/UserService.java
+++ src/main/java/com/chatting/chatting/service/UserService.java
@@ -1,4 +1,12 @@
 package com.chatting.chatting.service;
 
+import com.chatting.chatting.dto.UserDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    UserDTO setUser(UserDTO userDTO);
+    List<UserDTO> findAllUser();
+    Optional<UserDTO> findUserByName(String nickname);
 }
--- src/main/java/com/chatting/chatting/service/UserServiceImpl.java
+++ src/main/java/com/chatting/chatting/service/UserServiceImpl.java
@@ -1,7 +1,43 @@
 package com.chatting.chatting.service;
 
+import com.chatting.chatting.dto.UserDTO;
+import com.chatting.chatting.entity.User;
+import com.chatting.chatting.mapper.UserMapper;
+import com.chatting.chatting.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDTO setUser(UserDTO userDTO) {
+        User user = userMapper.toUser(userDTO);
+        User savedUser = userRepository.save(user);
+
+        return userMapper.toUserDTO(savedUser);
+    }
+
+    @Override
+    public List<UserDTO> findAllUser() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toUserDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<UserDTO> findUserByName(String nickname) {
+        return userRepository.findByNickname(nickname)
+                .map(userMapper::toUserDTO);
+    }
+
 }
