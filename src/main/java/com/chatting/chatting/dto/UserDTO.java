79cbc39a85b7bb2888b72a66f5064fcfe9d47e8e
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-06 19:33:59 +0900

    feat: 유저 필드 추가

--- src/main/java/com/chatting/chatting/dto/UserDTO.java
+++ src/main/java/com/chatting/chatting/dto/UserDTO.java
@@ -1,4 +1,16 @@
 package com.chatting.chatting.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+    private String id;
+    private String nickname;
+    private String password;
 }
